fcef37553d58b16e6785025b6f839da63f0c2ab6
haxwithaxe <me@haxwithaxe.net>
2013-07-06 13:19:03 -0400

    the source of the updated logging

--- byzantium/utils.py
+++ byzantium/utils.py
@@ -2,6 +2,7 @@
 # vim: set expandtab tabstop=4 shiftwidth=4 :
 
 import os
+import sys
 import logging
 import subprocess
 from . import convert2type, mknum
@@ -10,19 +11,52 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
+def __set_file_logging(logger, filename, level):
+    log_file_handler = logging.FileHandler(filename)
+    log_file_handler.setLevel(level)
+    logger.addHandler(log_file_handler)
+
+def __set_stderr_logging(logger, level):
+    log_stream_handler = logging.StreamHandler(sys.stderr)
+    log_stream_handler.setLevel(level)
+    logger.addHandler(log_stream_handler)
+
 class Utils:
     def __init__(self):
-        self.logger = logging
+        self.logger = logging.getLogger('python-byzantium')
         if 'BYZ_DEBUG' in os.environ and os.environ['BYZ_DEBUG']:
-            self.logger.basicConfig(level=logging.DEBUG)
+            self.log_level = logging.DEBUG
         else:
-            self.logger.basicConfig(level=logging.ERROR)
+            self.log_level = logging.ERROR
+        self.__set_file_logging(self.logger, '/tmp/byantium.log', self.log_level)#const.get('global-log-file'), self.log_level)
+        self.__set_stderr_logging(self.logger, self.log_level)
+        self.logger.debug('Utils.__init__')
 
     def __call__(self):
+        self.logger.debug('called utils')
         return self
 
-    def get_logger(self):
-        return self.logger
+    def __set_file_logging(self, logger, filename, level):
+        log_file_handler = logging.FileHandler(filename)
+        log_file_handler.setLevel(level)
+        logger.addHandler(log_file_handler)
+
+    def __set_stderr_logging(self, logger, level):
+        log_stream_handler = logging.StreamHandler(sys.stderr)
+        log_stream_handler.setLevel(level)
+        logger.addHandler(log_stream_handler)
+
+    def get_logger(self, name=None, level=None, filename=None, new=False):
+        self.logger.debug('got logger')
+        if not new: return self.logger
+        if name:
+            logger = logging.getLogger(name)
+        else:
+            logger = logging.getLogger()
+        if not level: level = self.log_level
+        logger.setLevel(level)
+        if filename: self.__set_file_logging(logger, filename, level)
+        return logger
 
     def get_mesh_ip(self):
         ip = ''
@@ -53,12 +87,16 @@ class Utils:
         conpar = configparser.SafeConfigParser()
         conpar.read(filename)
         config = {}
+        logging.debug('sections %s' % repr(conpar.sections()))
         for sec in conpar.sections():
+            logging.debug('sec, conpar.items(sec): %s\n%s' % (sec, repr(conpar.items(sec))))
             config[sec] = {}
             for k,v in conpar.items(sec):
+                logging.debug('for k, v in conpar.items(sec): k, v: %s, %s' % (k, v))
                 config[sec][k] = convert2type(v)
         if section:
-            config = config[section]
+            config.update(config[section])
+            logging.debug('config.__call__: %s' % repr(config))
         return config
 
     def dict2ini(self, input_dict, filename):
